4246eb6e393ff4070e3564b1cbb01bd950958f44
rise0chen <1034018425@qq.com>
2017-08-05 18:12:20 +0800

    若文件存在，直接重命名为'sync-[num]-[name]'

--- index.js
+++ index.js
@@ -1,65 +1,50 @@
-var fs  = require('fs');
+var fs = require('hexo-fs');
 var request = require('request');
 
+var host='http://tool.crise.cn/Hexo/write';
+var postDir = hexo.source_dir+'/_posts/';
 var file={};
-var state=0;
-var path='./source/_posts/';
-var host='http://115.159.216.99/Tool/Hexo/write/';
+
 requestTask = function(){
-  request(host+'f/g', function (error, response, content) {
+  request(host+'?f=g', function (error, response, content) {
     if(!error && (response.statusCode==200)){
       if(content.charCodeAt(0) == 0xFEFF){//rm bom
         content = content.slice(1);
       }
       //console.log('content:'+content);
-      if(content=='null'){
-        if(state==0){
-          console.log('\nFinish.');
-        }else{
-          console.log('\nyou can exec "hexo sync [web file name] -r [new file name]" to rename it.');
-          console.log('Or exec "hexo sync [web file name] -c" to cover it.');
+      JSON.parse(content, function(k, v) {
+        if(k=='id'){
+          file['id']=v;
+        }else if(k=='filename'){
+          file['name']=v;
+        }else if(k=='content'){
+          if(fs.existsSync(postDir+file['name'])){
+            var i=1;
+            while(fs.existsSync(postDir+'sync-'+i+'-'+file['name'])){i++;}
+            file['name']='sync-'+i+'-'+file['name'];
+          }
+          fs.writeFile(postDir+file['name'], v, (err)=>{
+            if(err){
+              console.log(err);
+            }else{
+              request(host+'?f=d&id='+file['id'], requestTask);
+              console.log('Success: wrote "'+file['name']+'"');
+            }
+          });
+        }else if(k=='error'){
+          if(v=='null'){
+            console.log('\nFinish.');
+          }else{
+            console.log('\n'+v);
+          }
         }
-      }else{
-        JSON.parse(content, function(k, v) {
-          if(k=='id'){
-            file['id']=v;
-          }
-          if(k=='filename'){
-            file['name']=v;
-          }
-          if(k=='content'){
-            fs.access(path+file['name'], (err) => {
-              if(!err){
-                state=1;
-                console.log('Warning: "'+file['name']+'" already exists!');
-              }else{
-                fs.writeFile(path+file['name'], v, (err)=>{
-                  if(err){
-                    console.log(err);
-                  }else{
-                    request(host+'f/d/id/'+file['id'], requestTask);
-                    console.log('Success: wrote "'+file['name']+'"');
-                  }
-                });
-              }
-            });
-          }
-        });
-      }
+      });
+    }else{
+      console.log("Can't connect net.");
     }
   });
 }
-var consoleOpts = {
-  usage: 'sync [web file name] [options]',
-  options: [
-    {name: '-r, --rename', desc: 'rename file'},
-    {name: '-c, --cover',  desc: 'cover file'}
-  ]
-};
+var consoleOpts = {};
 hexo.extend.console.register('sync', 'Sync writing what you post on web', consoleOpts, function(args){
-  if(args.r || args.rename){
-    console.log('\nrename.');
-  }else if (args.c || args.cover){
-    console.log('\ncover.');
-  }else{requestTask();}
+  requestTask();
 });
